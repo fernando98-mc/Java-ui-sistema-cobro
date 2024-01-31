10e5b0136e56584d7b7e7a1339853e105654be5f
fernando98-mc <luisf543678@gmail.com>
2024-01-31 12:32:22 -0600

    uploading first changes ☕🚀

---
+++ src/main/java/mx/com/app/sistema/de/cobro/SistemaDeCobro.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package mx.com.app.sistema.de.cobro;
+
+/**
+ *
+ * @author Userr
+ */
+public class SistemaDeCobro {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
